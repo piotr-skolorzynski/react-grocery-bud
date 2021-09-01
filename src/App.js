9468cb353da0e5e8e72ab2a0fc7e718e478a848c
Piotr <p.skolorzynski@gmail.com>
2021-09-01 14:34:50 +0200

    modify App.js set values in component add structure to render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-
-
 import { useState, useEffect } from 'react';
 import List from './List';
 import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEdditing, setIsEdditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    
+  }
+  
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            className="grocery" 
+            placeholder="e.g. eggs" 
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">{ isEdditing ? 'edit' : 'submit' }</button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+
+    </section>
+  );
 }
 
-export default App
+export default App;
